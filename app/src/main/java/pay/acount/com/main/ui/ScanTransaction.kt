14df0861083007a44aee450c1544ec1730d6d90e
Jonathan <jono1408@gmail.com>
2018-04-19 10:58:12 +0100

    First commit

---
+++ app/src/main/java/pay/acount/com/main/ui/ScanTransaction.kt
@@ -0,0 +1,22 @@
+package pay.acount.com.main.ui
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+
+/**
+ * Created by Jonathan on 09/04/2018.
+ */
+class ScanTransaction : ConstraintLayout {
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context, attrs, defStyleAttr)
+    }
+
+    fun init(context: Context, attrs: AttributeSet, defStyleAttr: Int) {
+
+    }
+
+
+}
