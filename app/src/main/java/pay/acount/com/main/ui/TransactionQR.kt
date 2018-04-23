90ff25b21752b4f34260fae93d3c17be36513723
Jonathan <jono1408@gmail.com>
2018-04-23 15:53:16 +0100

    missing commit

---
+++ app/src/main/java/pay/acount/com/main/ui/TransactionQR.kt
@@ -0,0 +1,43 @@
+package pay.acount.com.main.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+import com.google.gson.Gson
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import pay.acount.com.main.model.CustsomerTransactions
+
+/**
+ * Created by Jonathan on 09/04/2018.
+ *
+ * Create a QR code based on customer transaction details
+ *
+ */
+class TransactionQR : ImageView {
+
+    private lateinit var customerTransaction: CustsomerTransactions
+
+
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+
+    }
+
+
+    fun generateTransactioQrCode(custsomerTransactions: CustsomerTransactions) {
+        this.customerTransaction = customerTransaction
+        var qrText = Gson().toJson(customerTransaction)
+
+        val multiFormatWriter = MultiFormatWriter()
+
+        var bitmatrix = multiFormatWriter.encode(qrText, BarcodeFormat.QR_CODE, measuredWidth, measuredHeight)
+        var barcodeEncoer = BarcodeEncoder()
+        setImageBitmap(barcodeEncoer.createBitmap(bitmatrix))
+
+    }
+
+
+}
